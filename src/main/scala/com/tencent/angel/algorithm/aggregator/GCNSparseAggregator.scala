b016a61d2f2db2e0e8a23aaa5e60cd919ed6c510
yaochi <yaochi@sugo.io>
2019-09-16 15:07:16 +0800

    implement gcn and mean sparse aggregators

--- src/main/scala/com/tencent/angel/algorithm/aggregator/GCNSparseAggregator.scala
+++ src/main/scala/com/tencent/angel/algorithm/aggregator/GCNSparseAggregator.scala
@@ -1,16 +1,32 @@
 package com.tencent.angel.algorithm.aggregator
 
 import com.intel.analytics.bigdl.nn.Graph.ModuleNode
+import com.intel.analytics.bigdl.nn.keras.KerasLayerWrapper
+import com.intel.analytics.bigdl.nn.ops.Sum
+import com.intel.analytics.bigdl.nn.{CAddTable, CDivTable, MM}
 import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
+import com.intel.analytics.zoo.pipeline.api.keras.layers.{AddConstant, HardTanh}
 
 import scala.reflect.ClassTag
 
 class GCNSparseAggregator[T: ClassTag](dim: Int,
                                        activation: String = "relu",
-                                       concat: Boolean = false)
+                                       renorm: Boolean = false)
                                       (implicit ev: TensorNumeric[T]) extends BaseSparseAggregator[T] {
   override def aggregate(input: ModuleNode[T], neighbor: ModuleNode[T], adj: ModuleNode[T]): ModuleNode[T] = {
-    null
+    val degree = new KerasLayerWrapper[T](Sum[T, T]()).inputs(adj)
+    val aggregated = new KerasLayerWrapper[T](MM[T]()).inputs(neighbor, degree)
+
+    if (renorm) {
+      val merged = new KerasLayerWrapper[T](CAddTable[T]()).inputs(input, aggregated)
+      val norm = AddConstant(1).inputs(degree)
+      new KerasLayerWrapper[T](CDivTable[T]()).inputs(aggregated, norm)
+    } else {
+      val norm = HardTanh[T](minValue = 1e-7,
+        maxValue = Double.MaxValue).inputs(degree)
+      val normalized = new KerasLayerWrapper[T](CDivTable[T]()).inputs(aggregated, norm)
+      new KerasLayerWrapper[T](CAddTable[T]()).inputs(input, normalized)
+    }
   }
 }
 
--- src/main/scala/com/tencent/angel/algorithm/aggregator/MeanSparseAggregator.scala
+++ src/main/scala/com/tencent/angel/algorithm/aggregator/MeanSparseAggregator.scala
@@ -1,7 +1,11 @@
 package com.tencent.angel.algorithm.aggregator
 
 import com.intel.analytics.bigdl.nn.Graph.ModuleNode
+import com.intel.analytics.bigdl.nn.{CDivTable, MM}
+import com.intel.analytics.bigdl.nn.keras.KerasLayerWrapper
+import com.intel.analytics.bigdl.nn.ops.Sum
 import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
+import com.intel.analytics.zoo.pipeline.api.keras.layers.{Dense, HardTanh, Merge}
 
 import scala.reflect.ClassTag
 
@@ -10,6 +14,18 @@ class MeanSparseAggregator[T: ClassTag](dim: Int,
                                         concat: Boolean = false)
                                        (implicit ev: TensorNumeric[T]) extends BaseSparseAggregator[T] {
   override def aggregate(input: ModuleNode[T], neighbor: ModuleNode[T], adj: ModuleNode[T]): ModuleNode[T] = {
-    null
+    val degree = new KerasLayerWrapper[T](Sum[T, T]()).inputs(adj)
+    val norm = HardTanh[T](minValue = 1e-7,
+      maxValue = Double.MaxValue).inputs(degree)
+
+    val aggregated = new KerasLayerWrapper[T](MM[T]()).inputs(neighbor, degree)
+    val normalized = new KerasLayerWrapper[T](CDivTable[T]()).inputs(aggregated, norm)
+
+    val inputEmbedding = Dense(dim, activation = activation, bias = false).inputs(input)
+    val neighborEmbedding = Dense(dim, activation = activation, bias = false).inputs(aggregated)
+
+    val mode = if (concat) "concat" else "sum"
+
+    Merge[T](mode = mode).inputs(inputEmbedding, neighborEmbedding)
   }
 }
